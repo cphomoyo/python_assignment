c5285d1bffb8ca6ffd415d618cfe1a16f1d4b74d
cphomoyo@yahoo.com <sipho@Precious.(none)>
2014-04-06 21:37:06 +0200

    changes in question8

--- question8.py
+++ question8.py
@@ -7,7 +7,7 @@ if n<2:
 # 2 is the only even prime number
 if n == 2:
 	print "2 is a prime number"
-elif n%2==0 & n%3==0 & n%5==0:
+elif n%2!=0 and n%3!=0 and n%5!=0:
 	print n,"is a prime number"
 else:
 	print n, "is not a prime number"
